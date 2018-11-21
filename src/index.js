ae9b2e2f23e4c68b7cb9b5bc3bebd64042eb1eeb
epiqueras <epiquerass@gmail.com>
2018-11-21 10:10:04 +0000

    feat: start interfacing with contracts and web3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+import { Button } from 'antd'
+import { Drizzle, generateStore } from 'drizzle'
+import { DrizzleContext } from 'drizzle-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Button } from 'antd'
+
+import KlerosLiquid from './assets/contracts/kleros-liquid.json'
+import Pinakion from './assets/contracts/pinakion.json'
 
 import styled from 'styled-components/macro'
 
 import 'antd/dist/antd.css'
+
+const options = {
+  contracts: [
+    {
+      ...KlerosLiquid,
+      networks: { 42: { address: process.env.KLEROS_LIQUID_KOVAN_ADDRESS } }
+    },
+    {
+      ...Pinakion,
+      networks: { 42: { address: process.env.PINAKION_KOVAN_ADDRESS } }
+    }
+  ]
+}
+const drizzle = new Drizzle(options, generateStore(options))
 
 const HelloWorld = styled(({ className }) => (
   <div className={className}>
@@ -14,4 +33,9 @@ const HelloWorld = styled(({ className }) => (
   border: 2px solid red;
   width: 110px;
 `
-ReactDOM.render(<HelloWorld />, document.getElementById('root'))
+const App = () => (
+  <DrizzleContext.Provider drizzle={drizzle}>
+    <HelloWorld />
+  </DrizzleContext.Provider>
+)
+ReactDOM.render(<App />, document.getElementById('root'))
