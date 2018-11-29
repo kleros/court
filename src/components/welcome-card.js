d082985227655c87a9b1f61c2d38707d664b38fa
epiqueras <epiquerass@gmail.com>
2018-11-29 06:16:58 +0000

    feat(welcome-card): finish

--- src/components/welcome-card.js
+++ src/components/welcome-card.js
@@ -3,28 +3,47 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components/macro'
 
+const StyledCard = styled(Card)`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  z-index: 1;
+`
 const StyledCardGrid = styled(Card.Grid)`
   align-items: center;
   display: flex;
   height: 325px;
   justify-content: center;
+  position: relative;
 `
 const StyledIconCardGrid = styled(StyledCardGrid)`
+  background: white;
   border-radius: 12px;
   width: 393px;
 `
+const StyledDiv = styled.div`
+  bottom: 25px;
+  position: absolute;
+`
 const StyledTextCardGrid = styled(StyledCardGrid)`
   border-radius: 0 12px 12px 0;
+  color: white;
+  font-size: 48px;
+  font-weight: medium;
+  margin-left: -20px;
   width: 413px;
+  z-index: -1;
 `
 const WelcomeCard = ({ icon, text, version }) => (
-  <Card>
+  <StyledCard className="secondary">
     <StyledIconCardGrid>
       {icon}
-      {version}
+      <StyledDiv>{version}</StyledDiv>
     </StyledIconCardGrid>
-    <StyledTextCardGrid>{text}</StyledTextCardGrid>
-  </Card>
+    <StyledTextCardGrid className="theme-linear-background">
+      {text}
+    </StyledTextCardGrid>
+  </StyledCard>
 )
 
 WelcomeCard.propTypes = {
