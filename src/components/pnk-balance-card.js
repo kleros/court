cfdaf6f4450523f9703b8fbd5d8dfd2aa079f3ae
epiqueras <epiquerass@gmail.com>
2018-12-04 14:30:09 +0000

    feat(pnk-balance-card): finish data fetching and loading states

--- src/components/pnk-balance-card.js
+++ src/components/pnk-balance-card.js
@@ -1,4 +1,4 @@
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Row, Spin } from 'antd'
 import ETHAddress from './eth-address'
 import ETHAmount from './eth-amount'
 import Identicon from '../components/identicon'
@@ -49,52 +49,64 @@ const StyledSectionArrowBackground = styled(SectionArrowBackground)`
 `
 const PNKBalanceCard = () => {
   const { cacheCall, drizzleState } = useDrizzle()
+  const juror = cacheCall('KlerosLiquid', 'getJuror', drizzleState.accounts[0])
   return (
     <StyledCard hoverable>
-      <Row>
-        <Col span={8}>
-          <Row>
-            <Col span={10}>
-              <Identicon large />
-            </Col>
-            <Col span={14}>
-              <StyledDiv>
-                <ETHAddress address={drizzleState.accounts[0]} />
-              </StyledDiv>
-              <StyledDiv>
-                <ETHAmount
-                  amount={cacheCall(
-                    'MiniMeTokenERC20',
-                    'balanceOf',
-                    drizzleState.accounts[0]
-                  )}
-                />{' '}
-                PNK
-              </StyledDiv>
-              <StyledDiv>
-                <ETHAmount
-                  amount={
-                    drizzleState.accountBalances[drizzleState.accounts[0]]
-                  }
-                />{' '}
-                ETH
-              </StyledDiv>
-            </Col>
-          </Row>
-          <StyledSectionArrow className="ternary" />
-        </Col>
-        <Col className="ternary theme-color" span={8}>
-          <StyledTopDiv>You have</StyledTopDiv>
-          <StyledCenterDiv>500 PNK</StyledCenterDiv>
-          <StyledBottomDiv>Staked</StyledBottomDiv>
-        </Col>
-        <StyledCol className="ternary theme-color" span={8}>
-          <StyledTopDiv>You have</StyledTopDiv>
-          <StyledCenterDiv>100 PNK</StyledCenterDiv>
-          <StyledBottomDiv>Locked</StyledBottomDiv>
-          <StyledSectionArrowBackground />
-        </StyledCol>
-      </Row>
+      <Spin
+        spinning={
+          !drizzleState.contracts.KlerosLiquid.synced ||
+          !drizzleState.contracts.MiniMeTokenERC20.synced
+        }
+      >
+        <Row>
+          <Col span={8}>
+            <Row>
+              <Col span={10}>
+                <Identicon large />
+              </Col>
+              <Col span={14}>
+                <StyledDiv>
+                  <ETHAddress address={drizzleState.accounts[0]} />
+                </StyledDiv>
+                <StyledDiv>
+                  <ETHAmount
+                    amount={cacheCall(
+                      'MiniMeTokenERC20',
+                      'balanceOf',
+                      drizzleState.accounts[0]
+                    )}
+                  />{' '}
+                  PNK
+                </StyledDiv>
+                <StyledDiv>
+                  <ETHAmount
+                    amount={
+                      drizzleState.accountBalances[drizzleState.accounts[0]]
+                    }
+                  />{' '}
+                  ETH
+                </StyledDiv>
+              </Col>
+            </Row>
+            <StyledSectionArrow className="ternary" />
+          </Col>
+          <Col className="ternary theme-color" span={8}>
+            <StyledTopDiv>You have</StyledTopDiv>
+            <StyledCenterDiv>
+              <ETHAmount amount={juror && juror.stakedTokens} /> PNK
+            </StyledCenterDiv>
+            <StyledBottomDiv>Staked</StyledBottomDiv>
+          </Col>
+          <StyledCol className="ternary theme-color" span={8}>
+            <StyledTopDiv>You have</StyledTopDiv>
+            <StyledCenterDiv>
+              <ETHAmount amount={juror && juror.lockedTokens} /> PNK
+            </StyledCenterDiv>
+            <StyledBottomDiv>Locked</StyledBottomDiv>
+            <StyledSectionArrowBackground />
+          </StyledCol>
+        </Row>
+      </Spin>
     </StyledCard>
   )
 }
