4b682379ad95ac53aabea4b1a4fe7d9c147556fc
epiqueras <epiquerass@gmail.com>
2018-12-13 15:34:06 +0000

    feat(cases-list-card): load data

--- src/components/cases-list-card.js
+++ src/components/cases-list-card.js
@@ -4,7 +4,6 @@ import TitledListCard from './titled-list-card'
 import styled from 'styled-components/macro'
 import { useDrizzle } from '../temp/drizzle-react-hooks'
 
-// TODO
 const StyledListItem = styled(List.Item)`
   font-weight: bold;
   padding-left: 19px;
@@ -22,26 +21,33 @@ const CasesListCard = () => {
       [drizzleState.accounts[0]]
     )
   )
-  const counts =
-    draws &&
-    Object.entries(
-      draws.reduce((acc, d) => {
-        if (acc[d.returnValues._disputeID]) acc[d.returnValues._disputeID]++
-        else acc[d.returnValues._disputeID] = 1
-        return acc
-      }, {})
-    ).reduce(
-      (acc, d) => {
-        const dispute = cacheCall('KlerosLiquid', 'disputes', d[0])
-        if (dispute) acc[dispute.period === 4 ? 1 : 0]++
-        return acc
-      },
-      [0, 0]
-    )
+  const disputes = draws
+    ? draws.reduce(
+        (acc, d) => {
+          if (!acc.IDs[d.returnValues._disputeID]) {
+            acc.IDs[d.returnValues._disputeID] = true
+            acc.total++
+            const dispute = cacheCall(
+              'KlerosLiquid',
+              'disputes',
+              d.returnValues._disputeID
+            )
+            if (dispute) acc[dispute.period === 4 ? 'executed' : 'active']++
+            else acc.loading = true
+          }
+          return acc
+        },
+        { IDs: {}, active: 0, executed: 0, loading: false, total: 0 }
+      )
+    : { loading: true }
   return (
-    <TitledListCard prefix={draws && draws.length} title="Cases">
-      <StyledListItem extra={draws && counts[0]}>Active</StyledListItem>
-      <StyledListItem extra={draws && counts[1]}>Executed</StyledListItem>
+    <TitledListCard
+      loading={disputes.loading}
+      prefix={disputes.total}
+      title="Cases"
+    >
+      <StyledListItem extra={disputes.active}>Active</StyledListItem>
+      <StyledListItem extra={disputes.executed}>Executed</StyledListItem>
     </TitledListCard>
   )
 }
