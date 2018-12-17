a1bc04e9a4bd7b2ad8c3ab00695896b25f7f053d
epiqueras <epiquerass@gmail.com>
2018-12-17 17:37:45 +0000

    feat(cases-list-card): display closest deadline

--- src/components/cases-list-card.js
+++ src/components/cases-list-card.js
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react'
+import { ReactComponent as Hourglass } from '../assets/images/hourglass.svg'
 import { List } from 'antd'
+import TimeAgo from './time-ago'
 import TitledListCard from './titled-list-card'
 import styled from 'styled-components/macro'
 import { useDrizzle } from '../temp/drizzle-react-hooks'
@@ -16,6 +18,24 @@ const StyledListItem = styled(List.Item)`
     top: 50%;
     transform: translateY(-50%);
   }
+`
+const StyledDiv = styled.div`
+  background: whitesmoke;
+  padding: 30px 22px;
+  position: relative;
+  text-align: center;
+`
+const StyledDeadlineDiv = styled.div`
+  font-weight: medium;
+`
+const StyledTimeAgo = styled(TimeAgo)`
+  font-size: 24px;
+  font-weight: bold;
+`
+const StyledHourglass = styled(Hourglass)`
+  position: absolute;
+  right: 13px;
+  top: 13px;
 `
 const CasesListCard = () => {
   const { cacheCall, drizzleState, useCacheEvents } = useDrizzle()
@@ -41,8 +61,25 @@ const CasesListCard = () => {
               'disputes',
               d.returnValues._disputeID
             )
-            if (dispute) acc[dispute.period === 4 ? 'executed' : 'active']++
-            else acc.loading = true
+            if (dispute) {
+              acc[dispute.period === '4' ? 'executed' : 'active']++
+              if (dispute.period === '1' || dispute.period === '2') {
+                const subcourt = cacheCall(
+                  'KlerosLiquid',
+                  'getSubcourt',
+                  dispute.subcourtID
+                )
+                if (subcourt) {
+                  const deadline = new Date(
+                    (Number(dispute.lastPeriodChange) +
+                      Number(subcourt.timesPerPeriod[dispute.period])) *
+                      1000
+                  )
+                  if (!acc.deadline || deadline < acc.deadline)
+                    acc.deadline = deadline
+                } else acc.loading = true
+              }
+            } else acc.loading = true
           }
           return acc
         },
@@ -59,6 +96,13 @@ const CasesListCard = () => {
       <StyledListItem extra={String(disputes.executed)}>
         Executed
       </StyledListItem>
+      {disputes.deadline && (
+        <StyledDiv className="primary-color theme-color">
+          <StyledDeadlineDiv>Next voting deadline</StyledDeadlineDiv>
+          <StyledTimeAgo>{disputes.deadline}</StyledTimeAgo>
+          <StyledHourglass className="primary-fill" />
+        </StyledDiv>
+      )}
     </TitledListCard>
   )
 }
