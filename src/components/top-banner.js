e9f6b58a6148083773be8d2386d92ba59956b758
epiqueras <epiquerass@gmail.com>
2019-01-18 11:17:36 +0000

    feat(case): pull data for header

--- src/components/top-banner.js
+++ src/components/top-banner.js
@@ -7,9 +7,6 @@ import styled from 'styled-components/macro'
 const StyledCard = styled(Card)`
   margin: 0 -9.375vw 28px;
 `
-const StyledRow = styled(Row)`
-  line-height: 32px;
-`
 const StyledTitleCol = styled(Col)`
   font-weight: bold;
 `
@@ -20,21 +17,25 @@ const StyledUnderline = styled(Underline)`
 `
 const TopBanner = ({ description, extra, title }) => (
   <StyledCard>
-    <StyledRow>
+    <Row align="middle" type="flex">
       <StyledTitleCol offset={2} span={6}>
         {title}
         <StyledUnderline className="primary-fill" />
       </StyledTitleCol>
       <Col span={12}>{description}</Col>
       <Col span={4}>{extra}</Col>
-    </StyledRow>
+    </Row>
   </StyledCard>
 )
 
 TopBanner.propTypes = {
-  description: PropTypes.string.isRequired,
-  extra: PropTypes.node.isRequired,
+  description: PropTypes.node.isRequired,
+  extra: PropTypes.node,
   title: PropTypes.string.isRequired
+}
+
+TopBanner.defaultProps = {
+  extra: null
 }
 
 export default TopBanner
--- src/containers/case.js
+++ src/containers/case.js
@@ -1,12 +1,96 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useDrizzle, useDrizzleState } from '../temp/drizzle-react-hooks'
+import ETHAmount from '../components/eth-amount'
+import TimeAgo from '../components/time-ago'
 import TopBanner from '../components/top-banner'
+import styled from 'styled-components/macro'
 
-export default () => (
-  <>
-    <TopBanner
-      description="Case #1 | 200 PNK Locked"
-      extra="Deadline"
-      title="Case Details"
-    />
-  </>
-)
+const StyledDiv = styled.div`
+  font-weight: bold;
+`
+const StyledBigTextDiv = styled(StyledDiv)`
+  font-size: 20px;
+`
+export default ({
+  match: {
+    params: { ID }
+  }
+}) => {
+  const { drizzle, useCacheCall, useCacheEvents } = useDrizzle()
+  const drizzleState = useDrizzleState(drizzleState => ({
+    account: drizzleState.accounts[0]
+  }))
+  const dispute = useCacheCall('KlerosLiquid', 'disputes', ID)
+  const dispute2 = useCacheCall('KlerosLiquid', 'getDispute', ID)
+  const draws = useCacheEvents(
+    'KlerosLiquid',
+    'Draw',
+    useMemo(
+      () => ({
+        filter: { _address: drizzleState.account, _disputeID: ID },
+        fromBlock: 0
+      }),
+      [drizzleState.account, ID]
+    )
+  )
+  const disputeData = useCacheCall(['KlerosLiquid'], call => {
+    let disputeData = {}
+    if (dispute2 && draws) {
+      const jurorAtStake = dispute2.jurorAtStake.map(drizzle.web3.utils.toBN)
+      const votesByAppeal = draws.reduce((acc, d) => {
+        acc[d.returnValues._appeal] = acc[d.returnValues._appeal]
+          ? acc[d.returnValues._appeal].add(drizzle.web3.utils.toBN(1))
+          : drizzle.web3.utils.toBN(1)
+        return acc
+      }, {})
+      disputeData = Object.keys(votesByAppeal).reduce(
+        (acc, a) => {
+          acc.atStake = acc.atStake.add(votesByAppeal[a].mul(jurorAtStake[a]))
+          return acc
+        },
+        {
+          atStake: drizzle.web3.utils.toBN(0),
+          deadline: undefined
+        }
+      )
+      if (
+        dispute &&
+        dispute.period !== '4' &&
+        votesByAppeal[dispute2.votesLengths.length - 1]
+      ) {
+        const subcourt = call('KlerosLiquid', 'getSubcourt', dispute.subcourtID)
+        if (subcourt)
+          disputeData.deadline = new Date(
+            (Number(dispute.lastPeriodChange) +
+              Number(subcourt.timesPerPeriod[dispute.period])) *
+              1000
+          )
+      }
+    }
+    return disputeData
+  })
+  return (
+    <>
+      <TopBanner
+        description={
+          <>
+            Case #{ID} | <ETHAmount amount={disputeData.atStake} /> PNK Locked
+          </>
+        }
+        extra={
+          disputeData.deadline && (
+            <>
+              <StyledDiv>Voting Deadline</StyledDiv>
+              <StyledBigTextDiv>
+                <TimeAgo className="primary-color theme-color">
+                  {disputeData.deadline}
+                </TimeAgo>
+              </StyledBigTextDiv>
+            </>
+          )
+        }
+        title="Case Details"
+      />
+    </>
+  )
+}
