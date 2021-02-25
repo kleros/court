0f7756a1cafab0826210199cdbf10033afce04ee
Ferit Tunçer <ferit@cryptolab.net>
2021-02-25 15:35:16 +0300

    fix(claim-modal): disable button when less than one tokens and href for etherscan link

--- src/components/claim-modal.js
+++ src/components/claim-modal.js
@@ -18,11 +18,12 @@ const { useDrizzle, useDrizzleState } = drizzleReactHooks;
 const ClaimModal = ({ visible, onOk, onCancel }) => {
   const { drizzle, useCacheCall, useCacheSend } = useDrizzle();
   const drizzleState = useDrizzleState(drizzleState => ({
-    account: drizzleState.accounts[0] || VIEW_ONLY_ADDRESS
+    account: drizzleState.accounts[0] || VIEW_ONLY_ADDRESS,
+    web3: drizzleState.web3
   }));
 
   const [claims, setClaims] = useState(0);
-  const [pastClaims, setPastClaims] = useState(0);
+  const [txHash, setTxHash] = useState(null);
   const [claimStatus, setClaimStatus] = useState(0);
 
   const claimObjects = claims => {
@@ -36,6 +37,8 @@ const ClaimModal = ({ visible, onOk, onCancel }) => {
     for (var month = 0; month < 1; month++) {
       responses[month] = fetch(`https://ipfs.kleros.io/ipfs/QmX5hRNTtyLGmeG8fdRZKCZsJvR8HyEsjxm7XShS4Ga9hh`);
     }
+
+    console.log(drizzleState);
 
     const results = Promise.all(responses.map(promise => promise.then(r => r.json()).catch(e => console.error(e))));
 
@@ -61,7 +64,7 @@ const ClaimModal = ({ visible, onOk, onCancel }) => {
 
     claimStatus.then(r => setClaimStatus(r));
     // claimStatus.then(console.log);
-  }, [drizzleState.account, getTotalClaimable]);
+  }, [drizzleState.account]);
 
   const delay = delayInMilliseconds => new Promise(resolve => setTimeout(resolve, delayInMilliseconds));
 
@@ -70,11 +73,14 @@ const ClaimModal = ({ visible, onOk, onCancel }) => {
   const handleClaim = () => {
     setModalState(1);
 
-    claimWeeks(claims)
-      .then(handleClaimed)
-      .catch(err => {
-        setModalState(0);
-      });
+    const tx = claimWeeks(claims);
+    tx.on("transactionHash", function(hash) {
+      setTxHash(hash);
+    });
+
+    tx.then(handleClaimed).catch(err => {
+      setModalState(0);
+    });
   };
 
   const handleClaimed = () => {
@@ -161,20 +167,40 @@ const ClaimModal = ({ visible, onOk, onCancel }) => {
       {modalState >= 1 && <hr style={{ width: "100%", border: "1px solid rgba(0,0,0,0.1", marginBottom: "32px" }} />}
       {modalState == 2 && <div style={{ fontSize: "18px", fontWeight: "400" }}> Thank you for being part of the community! </div>}
       <div style={{ fontSize: "18px", color: "#009AFF" }}>
-        <a href={modalState == 0 ? "https://blog.kleros.io/tag/announcements/" : modalState == 1 ? "https://etherscan.io" : "kleros.io"} target="_blank" rel="noopener noreferrer">
-          {modalState == 0 ? "Read more about Justice Farming" : modalState == 1 ? "View Transaction on Etherscan" : "Read more about the Juror Incentive Program"} <RightArrow style={{ marginLeft: "4px", verticalAlign: "middle" }} />
-        </a>
+        {modalState == 0 && (
+          <a href="kleros.io" target="_blank" rel="noopener noreferrer">
+            Read more about Justice Farming <RightArrow style={{ marginLeft: "4px", verticalAlign: "middle" }} />
+          </a>
+        )}
+
+        {modalState == 1 && txHash && (
+          <a href={`https://${drizzleState.web3.networkId == 42 && "kovan."}etherscan.io/tx/${txHash}`} target="_blank" rel="noopener noreferrer">
+            View Transaction on Etherscan <RightArrow style={{ marginLeft: "4px", verticalAlign: "middle" }} />
+          </a>
+        )}
+
+        {modalState == 2 && (
+          <a href="kleros.io" target="_blank" rel="noopener noreferrer">
+            Read more about the Juror Incentive Program <RightArrow style={{ marginLeft: "4px", verticalAlign: "middle" }} />
+          </a>
+        )}
       </div>
-      {modalState == 0 && (
+      {modalState == 0 && claims && (
         <Button
           onClick={handleClaim}
           size="large"
           type="primary"
-          style={{
-            marginTop: "40px",
-            backgroundColor: "#9013FE",
-            border: "none"
-          }}
+          style={
+            Number(drizzle.web3.utils.fromWei(getTotalClaimable(claims))).toFixed(0) < 1
+              ? { marginTop: "40px", border: "none", color: "#CCC", backgroundColor: "#fafafa" }
+              : {
+                  marginTop: "40px",
+                  backgroundColor: "#9013FE",
+                  color: "white",
+                  border: "none"
+                }
+          }
+          disabled={Number(drizzle.web3.utils.fromWei(getTotalClaimable(claims))).toFixed(0) < 1}
         >
           Claim Your PNK Tokens
         </Button>
