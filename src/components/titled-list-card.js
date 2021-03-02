52ff45f39d8d4c94030518410a9425797446a69d
Ferit Tunçer <ferit@cryptolab.net>
2021-03-02 15:15:23 +0300

    refactor(titled-list-card): apy top px instead of perc

--- src/components/titled-list-card.js
+++ src/components/titled-list-card.js
@@ -1,11 +1,11 @@
-import { Card, Tooltip } from 'antd'
-import React, { Fragment } from 'react'
-import { ReactComponent as Hexagon } from '../assets/images/hexagon.svg'
-import { ReactComponent as Question } from '../assets/images/question-circle.svg'
+import { Card, Tooltip } from "antd";
+import React, { Fragment } from "react";
+import { ReactComponent as Hexagon } from "../assets/images/hexagon.svg";
+import { ReactComponent as Question } from "../assets/images/question-circle.svg";
 
-import PropTypes from 'prop-types'
-import { ReactComponent as Underline } from '../assets/images/underline.svg'
-import styled from 'styled-components/macro'
+import PropTypes from "prop-types";
+import { ReactComponent as Underline } from "../assets/images/underline.svg";
+import styled from "styled-components/macro";
 
 const StyledCard = styled(Card)`
   background: none;
@@ -39,7 +39,7 @@ const StyledCard = styled(Card)`
       }
     }
   }
-`
+`;
 const StyledPrefixDiv = styled.div`
   font-size: 21px;
   font-weight: bold;
@@ -47,7 +47,7 @@ const StyledPrefixDiv = styled.div`
   position: absolute;
   top: 51px;
   transform: translate(-50%, -50%);
-`
+`;
 const StyledTitleDiv = styled.div`
   font-size: 24px;
   font-weight: medium;
@@ -68,24 +68,24 @@ const StyledTitleDiv = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
-`
+`;
 const StyledUnderline = styled(Underline)`
   height: 4px;
   left: 0;
   position: absolute;
   top: 103px;
   width: 100%;
-`
+`;
 const StyledDivider = styled.div`
   border-bottom: 1px solid #d09cff;
   margin: 0;
   width: 100%;
-`
+`;
 
 const StyledTooltipDiv = styled.span`
   position: absolute;
   right: 18px;
-  top: 20%;
+  top: 42.5px;
   font-size: 16px;
   font-weight: 500;
   .ant-tooltip-arrow {
@@ -99,7 +99,7 @@ const StyledTooltipDiv = styled.span`
     min-width: 200px;
     padding: 16px;
   }
-`
+`;
 
 const TitledListCard = ({ children, loading, prefix, title, apy }) => (
   <StyledCard
@@ -109,18 +109,15 @@ const TitledListCard = ({ children, loading, prefix, title, apy }) => (
     title={
       <>
         {apy && (
-          <Tooltip
-            title="The current rate. Subject to change depending on total staked amount."
-            getPopupContainer={triggerNode => triggerNode}
-          >
+          <Tooltip title="The current rate. Subject to change depending on total staked amount." getPopupContainer={triggerNode => triggerNode}>
             <StyledTooltipDiv>
               {`${((1000000 / apy) * 12 * 100).toFixed(2)}% APY`}
               <Question
                 style={{
-                  verticalAlign: 'text-bottom',
-                  marginLeft: '8px',
-                  height: '19px',
-                  width: 'auto'
+                  verticalAlign: "text-bottom",
+                  marginLeft: "8px",
+                  height: "19px",
+                  width: "auto"
                 }}
               />
             </StyledTooltipDiv>
@@ -148,19 +145,19 @@ const TitledListCard = ({ children, loading, prefix, title, apy }) => (
             )
           ))}
   </StyledCard>
-)
+);
 
 TitledListCard.propTypes = {
   children: PropTypes.node,
   loading: PropTypes.bool,
   prefix: PropTypes.node,
   title: PropTypes.node.isRequired
-}
+};
 
 TitledListCard.defaultProps = {
   children: null,
   loading: false,
   prefix: null
-}
+};
 
-export default TitledListCard
+export default TitledListCard;
