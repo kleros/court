4b0460ba3d8f6c11b25f8863885e443153ec1bb9
alcercu <alcercu@alumni.upv.es>
2021-07-07 17:19:35 +0200

    fix: add more liquid markets and remove IDEX

--- src/components/pnk-exchanges-card.js
+++ src/components/pnk-exchanges-card.js
@@ -1,5 +1,4 @@
 import { ReactComponent as Bitfinex } from "../assets/images/bitfinex.svg";
-import { ReactComponent as Idex } from "../assets/images/idex.svg";
 import DeversiFi from "../assets/images/deversifi.png";
 import { ReactComponent as Uniswap } from "../assets/images/uniswap.svg";
 import { ReactComponent as Loopring } from "../assets/images/loopring.svg";
@@ -8,6 +7,9 @@ import Transak from "../assets/images/transak.png";
 import { ReactComponent as OneInch } from "../assets/images/OneInch.svg";
 import Paraswap from "../assets/images/paraswap.jpg";
 import { ReactComponent as Balancer } from "../assets/images/balancer.svg";
+import Sushiswap from "../assets/images/sushiswap.png";
+import { ReactComponent as GateIO } from "../assets/images/gateio.svg";
+import { ReactComponent as OKEX } from "../assets/images/okex.svg";
 import React from "react";
 import styled from "styled-components/macro";
 
@@ -37,44 +39,52 @@ const StyledExchangeSection = styled.div`
 
 const Exchanges = [
   {
-    logo: <Bitfinex />,
-    link: "https://www.bitfinex.com/t/PNKETH",
-  },
-  {
-    logo: <img src={DeversiFi} alt={"DeversiFi"} />,
-    link: "https://app.deversifi.com/",
-  },
-  {
     logo: <Uniswap />,
-    link: "https://uniswap.exchange/",
+    link: "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d",
   },
   {
-    logo: <Loopring />,
-    link: "https://loopring.org/",
+    logo: <img src={Sushiswap} alt={"Sushiswap"} />,
+    link: "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d",
   },
   {
-    logo: <Idex />,
-    link: "https://idex.market/eth/pnk",
+    logo: <Balancer />,
+    link: "https://balancer.exchange/",
   },
   {
     logo: <img src={KyberSwap} alt={"KyberSwap"} />,
     link: "https://kyberswap.com/swap",
   },
   {
-    logo: <img src={Transak} alt={"Transak"} />,
-    link: "https://transak.com/",
+    logo: <img src={DeversiFi} alt={"DeversiFi"} />,
+    link: "https://app.deversifi.com/",
   },
   {
-    logo: <OneInch />,
-    link: "https://1inch.exchange/",
+    logo: <Loopring />,
+    link: "https://loopring.org/",
   },
   {
     logo: <img src={Paraswap} alt={"Paraswap"} />,
     link: "https://paraswap.io/",
   },
   {
-    logo: <Balancer />,
-    link: "https://balancer.exchange/",
+    logo: <OneInch />,
+    link: "https://1inch.exchange/",
+  },
+  {
+    logo: <Bitfinex />,
+    link: "https://www.bitfinex.com/t/PNKETH",
+  },
+  {
+    logo: <GateIO />,
+    link: "https://www.gate.io/trade/PNK_USDT",
+  },
+  {
+    logo: <OKEX />,
+    link: "https://www.okex.com/markets/spot-info/pnk-usdt",
+  },
+  {
+    logo: <img src={Transak} alt={"Transak"} />,
+    link: "https://transak.com/",
   },
 ];
 
