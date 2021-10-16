18004601ac45fe143efa64342c0cd8f7afc7e18b
Henrique Barcelos <hbarcelos909@gmail.com>
2021-10-16 18:55:39 -0300

    refactor: make getMetaEvidence comply with new archon options
    This fixes an outstanding issue with cross-chain MetaEvidence by allowing
    archon to obtain a valid JSON-RPC URL for the chain where the arbitrable
    contract lives.
    
    Depends on @kleros/archon >= 2.2.0.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-const { useBabelRc, override } = require("customize-cra");
+const webpack = require("webpack");
+const { addWebpackPlugin, override, useBabelRc } = require("customize-cra");
 
-module.exports = override(useBabelRc());
+module.exports = override(useBabelRc(), addWebpackPlugin(new webpack.ContextReplacementPlugin(/v8-sandbox/)));
