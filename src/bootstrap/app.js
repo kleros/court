69331675d5bf137d42e0fe239893b6bbd38bac76
brainsale <1965270+brainsale@users.noreply.github.com>
2022-04-25 21:02:29 +0200

    No rewards claiming

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -10,7 +10,6 @@ import { BrowserRouter, NavLink, Route, Switch, useParams } from "react-router-d
 import { ReactComponent as Logo } from "../assets/images/kleros-logo-flat-light.svg";
 import AccountStatus from "../components/account-status";
 import Footer from "../components/footer";
-import NotificationSettings from "../components/notification-settings";
 import { ChainIdProvider } from "../hooks/use-chain-id";
 import { ArchonInitializer } from "./archon";
 import ChainChangeWatcher from "./chain-change-watcher";
@@ -60,7 +59,6 @@ export default function App() {
                           <StyledTrayCol lg={6} md={8} sm={12} xs={24}>
                             <StyledTray>
                               <AccountStatus />
-                              <NotificationSettings settings={settings} />
                             </StyledTray>
                           </StyledTrayCol>
                         </Row>
@@ -188,15 +186,6 @@ const MenuItems = [
     </a>
   </Menu.Item>,
 ];
-
-const settings = {
-  draw: "When I am drawn as a juror.",
-  appeal: "When a case I ruled is appealed.",
-  key: "court",
-  lose: "When I lose tokens.",
-  win: "When I win arbitration fees.",
-  stake: "When my stakes are changed.",
-};
 
 const StyledLayoutSider = styled(Layout.Sider)`
   height: 100%;
--- src/containers/home.js
+++ src/containers/home.js
@@ -11,7 +11,6 @@ import NotificationsCard from "../components/notifications-card";
 import OngoingCasesCard from "../components/ongoing-cases-card";
 import PerformanceCard from "../components/performance-card";
 import RewardCard from "../components/reward-card";
-import ClaimModal from "../components/claim-modal";
 import TopBanner from "../components/top-banner";
 import TokenSymbol from "../components/token-symbol";
 import RequiredChainIdGateway from "../components/required-chain-id-gateway";
@@ -22,7 +21,6 @@ import { ReactComponent as Present } from "../assets/images/present.svg";
 
 const { useDrizzleState } = drizzleReactHooks;
 
-const airdropChainIds = [1, 42, 77, 100];
 const buyPnkChainIds = [1, 100];
 
 export default function Home() {
@@ -35,93 +33,20 @@ export default function Home() {
 
   const isBuyPnkButtonVisible = buyPnkChainIds.includes(chainId);
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalButtonVisible, setIsModalButtonVisible] = useState(false);
-  const [apy, setApy] = useState(0);
 
   useEffect(() => {
     setIsModalButtonVisible(false);
   }, [drizzleState.account]);
 
-  const showModalButton = useCallback(() => {
-    setIsModalButtonVisible(true);
-  }, []);
-
-  const showModal = useCallback(() => {
-    setIsModalVisible(true);
-  }, []);
-
-  const handleOk = useCallback(() => {
-    setIsModalVisible(false);
-  }, []);
-
-  const handleCancel = useCallback(() => {
-    setIsModalVisible(false);
-  }, []);
-
-  const apyCallback = useCallback((apy) => {
-    setApy(apy);
-  }, []);
-
   const content = (
     <>
-      {airdropChainIds.includes(chainId) ? (
-        <ClaimModal
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          displayButton={showModalButton}
-          apyCallback={apyCallback}
-        />
-      ) : null}
       <TopBanner
         title="My Dashboard"
-        extra={
-          <StyledButtonBar>
-            <StyledClaimButton
-              onClick={showModal}
-              size="large"
-              style={{
-                display: isModalButtonVisible ? "inline-block" : "none",
-              }}
-              type="primary"
-            >
-              <Present style={{ marginRight: "8px", verticalAlign: "text-top" }} />
-              <span>
-                Claim <TokenSymbol token="PNK" />
-              </span>
-            </StyledClaimButton>
-            <Link
-              to="/tokens"
-              style={{
-                display: isBuyPnkButtonVisible ? "inline-block" : "none",
-              }}
-            >
-              <StyledButton size="large" type="secondary">
-                Buy PNK
-              </StyledButton>
-            </Link>
-            <Link to="/courts">
-              <StyledButton size="large" type="primary">
-                See Courts
-              </StyledButton>
-            </Link>
-          </StyledButtonBar>
-        }
+
       />
       <SwitchCourtChain />
       <RewardCard />
-      <Row gutter={32}>
-        <Col lg={8}>
-          <CourtsListCard apy={apy > 1000000 ? apy : null} />
-        </Col>
-        <Col lg={8}>
-          <CasesListCard />
-        </Col>
-        <Col lg={8}>
-          <PerformanceCard />
-        </Col>
-      </Row>
       <OngoingCasesCard />
       <NotificationsCard />
       <SideChainPnkActions showGetSideChainPnkModal={false} />
