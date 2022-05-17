92b7814889f5ad421361cbd9b618718102731883
alcercu <alcercu@alumni.upv.es>
2022-05-17 13:06:25 +0200

    refactor(withdraw-stpnk-card): use form validation and similar styling

--- src/containers/convert-pnk/wihdraw-stpnk-card.js
+++ src/containers/convert-pnk/wihdraw-stpnk-card.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components/macro";
-import { Card, Button, Input } from "antd";
+import Web3 from "web3";
+import { Card, Button, Form, InputNumber } from "antd";
 import stPNKAbi from "../../assets/contracts/wrapped-pinakion.json";
+import TokenSymbol from "../../components/token-symbol";
 import { drizzleReactHooks } from "@drizzle/react-plugin";
 import { VIEW_ONLY_ADDRESS } from "../../bootstrap/dataloader";
+
+const { fromWei } = Web3.utils;
 
 const StyledCard = styled(Card)`
   margin: 20px auto 0;
@@ -47,9 +51,9 @@ const StyledCard = styled(Card)`
   }
 `;
 
-const StyledRow = styled.div`
+const StyledCol = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
 `;
 
 const StyledButton = styled(Button)`
@@ -63,28 +67,115 @@ const StyledExplainerText = styled.p`
   text-align: center;
 `;
 
+const StyledFormItem = styled(Form.Item)`
+  width: 100%;
+  && {
+    .ant-input-number {
+      width: 100%;
+    }
+  }
+`;
+
+const StyledButtonLink = styled.button.attrs((...rest) => ({ ...rest, type: "button" }))`
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  font-weight: inherit;
+  display: inline-block;
+  color: #009aff;
+`;
+
+const StyledCompositeLabel = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  ${StyledButtonLink} {
+    margin-left: auto;
+  }
+`;
+
 const { useDrizzle, useDrizzleState } = drizzleReactHooks;
 
-const WithdrawStPnk = () => {
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some((field) => fieldsError[field]);
+}
+
+const WithdrawStPnkForm = Form.create()(({ form, maxAvailable, isSubmitting, disabled }) => {
   const { drizzle } = useDrizzle();
   const { account } = useDrizzleState((drizzleState) => ({
     account: drizzleState.accounts[0] || VIEW_ONLY_ADDRESS,
   }));
-  const [amount, setAmount] = useState(0);
-  const [submitting, isSubmitting] = useState(false);
+  const { validateFieldsAndScroll, getFieldDecorator, setFieldsValue, getFieldsError } = form;
+  const maxAvailableNumeric = Math.trunc(Number(fromWei(maxAvailable ?? "0")));
 
-  const handleWithdraw = async () => {
-    isSubmitting(true);
-    const stPNK = new drizzle.web3.eth.Contract(stPNKAbi.abi, process.env.REACT_APP_PINAKION_XDAI_ADDRESS);
-    const { toWei, toBN } = drizzle.web3.utils;
-    const actualAmount = toWei(toBN(amount));
-    try {
-      await stPNK.methods.withdraw(actualAmount).send({ from: account });
-    } finally {
-      isSubmitting(false);
-    }
-  };
+  const amountDecorator = getFieldDecorator("amount", {
+    rules: [
+      { required: true, message: "Amount is required." },
+      async function validateBalance(_, value) {
+        if (value > maxAvailableNumeric) {
+          throw new Error("Not enough available tokens.");
+        }
+      },
+    ],
+  });
 
+  const handleUseMaxClick = React.useCallback(() => {
+    setFieldsValue({ amount: maxAvailableNumeric });
+  }, [setFieldsValue, maxAvailableNumeric]);
+
+  const handleSubmit = React.useCallback(
+    async (evt) => {
+      evt.preventDefault();
+      validateFieldsAndScroll(async (err, values) => {
+        if (err) {
+          console.debug("Form validation error:", err);
+          return;
+        }
+        const stPNK = new drizzle.web3.eth.Contract(stPNKAbi.abi, process.env.REACT_APP_PINAKION_XDAI_ADDRESS);
+        const { toWei, toBN } = drizzle.web3.utils;
+        const actualAmount = toWei(toBN(values.amount));
+        await stPNK.methods.withdraw(actualAmount).send({ from: account });
+      });
+    },
+    [validateFieldsAndScroll, account]
+  );
+
+  return (
+    <div>
+      <Form hideRequiredMark layout="vertical" onSubmit={handleSubmit}>
+        <StyledCol>
+          <StyledFormItem
+            hasFeedback
+            label={
+              <StyledCompositeLabel>
+                <TokenSymbol chainId={100} token="PNK" />
+                <StyledButtonLink onClick={handleUseMaxClick}>use max.</StyledButtonLink>
+              </StyledCompositeLabel>
+            }
+          >
+            {amountDecorator(
+              <InputNumber placeholder="Amount to convert" min={0} max={maxAvailableNumeric} size="large" />
+            )}
+          </StyledFormItem>
+
+          <StyledButton
+            size="large"
+            block
+            htmlType="submit"
+            loading={isSubmitting}
+            disabled={disabled || isSubmitting || hasErrors(getFieldsError())}
+          >
+            Convert stPNK to xPNK
+          </StyledButton>
+        </StyledCol>
+      </Form>
+    </div>
+  );
+});
+
+const WithdrawStPnk = () => {
   return (
     <StyledCard title={<>Convert stPNK to xPNK</>} description={<></>}>
       <StyledExplainerText
@@ -94,21 +185,7 @@ const WithdrawStPnk = () => {
       >
         Use this if you only want to obtain xPNK, for example, for usage in Gnosis Chain exchanges.
       </StyledExplainerText>
-      <StyledRow>
-        <Input
-          size="large"
-          type="number"
-          value={amount}
-          onChange={(e) => {
-            const newAmount = Math.floor(Number(e.target.value));
-            if (!isNaN(newAmount)) setAmount(newAmount);
-          }}
-        />
-
-        <StyledButton size="large" block loading={submitting} disabled={submitting} onClick={handleWithdraw}>
-          Convert stPNK to xPNK
-        </StyledButton>
-      </StyledRow>
+      <WithdrawStPnkForm />
     </StyledCard>
   );
 };
