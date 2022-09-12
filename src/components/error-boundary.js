201db60a6703324da7210e23cde52ce0afe5849f
gratestas <stasgrate@gmail.com>
2022-09-12 09:36:19 +0300

    feat: add error boundary component

---
+++ src/components/error-boundary.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { withScope, captureException } from "@sentry/browser";
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError = () => {
+    return { hasError: true };
+  };
+  componentDidCatch(error, errorInfo) {
+    withScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+      captureException(error);
+    });
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+  render() {
+    const { hasError } = this.state;
+    const { fallback: Fallback, children } = this.props;
+    return hasError ? <Fallback /> : children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node,
+  fallback: PropTypes.any,
+};
