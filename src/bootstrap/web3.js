beed453c45bbb3d88befc630a8c3932fca77e2c0
gratestas <stasgrate@gmail.com>
2022-09-12 10:26:12 +0300

    fix(web3): migrate from legacy API

--- src/bootstrap/web3.js
+++ src/bootstrap/web3.js
@@ -2,8 +2,8 @@ import Web3 from "web3";
 
 let web3;
 
-if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
-  web3 = new Web3(window.web3.currentProvider);
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  web3 = new Web3(window.ethereum.currentProvider);
 } else if (process.env.REACT_APP_WEB3_FALLBACK_HTTPS_URL || process.env.REACT_APP_WEB3_FALLBACK_URL) {
   // Fallback provider.
   web3 = new Web3(process.env.REACT_APP_WEB3_FALLBACK_HTTPS_URL ?? process.env.REACT_APP_WEB3_FALLBACK_URL);
