b0718762a6c7025cd9244fe3f0fa5d1291a952f6
alcercu <333aleix333@gmail.com>
2023-06-20 11:27:03 +0200

    chore(lemon): change netlify function from js to ts

--- netlify/functions/get-justifications.ts
+++ netlify/functions/get-justifications.ts
@@ -1,10 +1,11 @@
-const { createClient } = require("@supabase/supabase-js");
+import { Handler } from '@netlify/functions';
+const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.DATALAKE_URL;
 const supabaseKey = process.env.DATALAKE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-const allowedNetworks = ["gnosischain", "ethereum", "chiado"];
+const allowedNetworks = ['gnosischain', 'ethereum', 'chiado'];
 
-export const handler = async (event) => {
+export const handler: Handler = async (event) => {
   const disputeID = parseInt(event.queryStringParameters.disputeID);
   const appeal = parseInt(event.queryStringParameters.appeal);
   const network = event.queryStringParameters.network;
@@ -12,30 +13,27 @@ export const handler = async (event) => {
   if (!allowedNetworks.includes(network) || isNaN(disputeID) || isNaN(appeal) || event.httpMethod !== "GET") {
     return {
       statusCode: 400,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify({
-        error:
-          "Invalid inputs. Network must be gnosischain, ethereum or chiado, and both disputeID and appeal must be valid numbers.",
-      }),
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'Invalid inputs. Network must be gnosischain, ethereum or chiado, and both disputeID and appeal must be valid numbers.' }),
     };
   }
 
   const { data, error } = await supabase
     .from(`${network}-justifications`)
-    .select("*")
-    .eq("disputeIDAndAppeal", `${disputeID}-${appeal}`);
+    .select('*')
+    .eq('disputeIDAndAppeal', `${disputeID}-${appeal}`);
 
   if (error) {
     return {
       statusCode: 500,
-      headers: { "Access-Control-Allow-Origin": "*" },
+      headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: error.message }),
     };
   }
 
   return {
     statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
+    headers: { 'Access-Control-Allow-Origin': '*' },
     body: JSON.stringify({
       payload: {
         justifications: data,
