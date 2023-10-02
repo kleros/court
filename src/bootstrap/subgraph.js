b1d0ce9b4adf453c097e60930da8d2e6ec66fd27
jaybuidl <jb@kleros.io>
2023-10-02 14:51:31 +0100

    chore: migration to production subgraphs under the kleros org account

--- src/bootstrap/subgraph.js
+++ src/bootstrap/subgraph.js
@@ -1,6 +1,6 @@
 export const displaySubgraph = {
-  1: "https://api.thegraph.com/subgraphs/name/greenlucid/kleros-display-mainnet",
+  1: "https://api.thegraph.com/subgraphs/name/kleros/kleros-display-mainnet",
   5: "https://api.thegraph.com/subgraphs/name/andreimvp/kleros-display-goerli",
-  100: "https://api.thegraph.com/subgraphs/name/greenlucid/kleros-display",
+  100: "https://api.thegraph.com/subgraphs/name/kleros/kleros-display-gnosis",
   11155111: "https://api.studio.thegraph.com/query/50849/kleros-sepolia-gf/version/latest",
 };
