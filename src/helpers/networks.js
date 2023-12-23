9ecc30924e8e6d946ba8c269174f7891d2a149c5
marino <102478601+kemuru@users.noreply.github.com>
2023-12-23 01:35:08 +0100

    chore(web): change gnosis chain name

--- src/helpers/networks.js
+++ src/helpers/networks.js
@@ -2,14 +2,14 @@ export const chainIdToNetworkName = {
   1: "Ethereum Mainnet",
   5: "Ethereum Testnet Görli",
   100: "Gnosis Chain",
-  10200: "xDAI Testnet Chiado",
+  10200: "Gnosis Testnet Chiado",
   11155111: "Ethereum Testnet Sepolia",
 };
 
 export const chainIdToNetworkShortName = {
   1: "Mainnet",
   5: "Görli",
-  100: "xDAI",
+  100: "Gnosis Chain",
   10200: "Chiado",
   11155111: "Sepolia",
 };
