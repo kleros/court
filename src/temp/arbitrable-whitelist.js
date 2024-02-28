bfb355ea78a835ffd8c2fc50ac6f08232f9ef8d0
Aleix <80388279+alcercu@users.noreply.github.com>
2024-02-28 11:41:48 +0100

    Update arbitrable-whitelist.js for Sepolia realitio

--- src/temp/arbitrable-whitelist.js
+++ src/temp/arbitrable-whitelist.js
@@ -51,6 +51,9 @@ const arbitrableWhitelist = {
     "0xe04f5791d671d5c4e08ab49b39807087b591ea3e",
     "0xf7de5537ecd69a94695fcf4bcdbdee6329b63322",
   ],
+  11155111: [
+    "0xe3705b31546eae49543e384d17cfc6a376560e28",
+  ],
 };
 
 export default arbitrableWhitelist;
