3ba0f88d8e75d9a8f4ec38aea8414bb9d0047db2
jaybuidl <jb@kleros.io>
2024-04-15 16:29:57 +0100

    fix: sepolia subgraph url

--- src/bootstrap/subgraph.js
+++ src/bootstrap/subgraph.js
@@ -2,5 +2,5 @@ export const displaySubgraph = {
   1: "https://api.thegraph.com/subgraphs/name/kleros/kleros-display-mainnet",
   5: "https://api.thegraph.com/subgraphs/name/andreimvp/kleros-display-goerli",
   100: "https://api.thegraph.com/subgraphs/name/kleros/kleros-display-gnosis",
-  11155111: "https://api.studio.thegraph.com/query/50849/kleros-sepolia-gf/version/latest",
+  11155111: "https://api.thegraph.com/subgraphs/name/kleros/kleros-display-sepolia",
 };
