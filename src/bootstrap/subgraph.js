0da67a3225a000f800db5971e2b1158cecc26f4e
alcercu <333aleix333@gmail.com>
2024-10-03 12:58:14 +0200

    fix: add chiado subgraph to bootstrap

--- src/bootstrap/subgraph.js
+++ src/bootstrap/subgraph.js
@@ -2,5 +2,6 @@ export const displaySubgraph = {
   1: "https://api.studio.thegraph.com/query/61738/kleros-display-mainnet/version/latest",
   5: "https://api.thegraph.com/subgraphs/name/andreimvp/kleros-display-goerli",
   100: "https://api.studio.thegraph.com/query/61738/kleros-display-gnosis/version/latest",
+  10200: "https://api.studio.thegraph.com/query/61738/kleros-display-chiado/version/latest",
   11155111: "https://api.studio.thegraph.com/query/61738/kleros-display-sepolia/version/latest",
 };
