f607f63eaddf5788f5570170f94ec9bcf4e38dae
kyrers <kyrers.eth@gmail.com>
2026-01-22 23:13:00 +0000

    chore: delete unused code

--- src/bootstrap/api.js
+++ src/bootstrap/api.js
@@ -1,53 +1,6 @@
 import axios from "axios";
 import web3DeriveAccount from "../temp/web3-derive-account";
 
-export const accessSettings = async ({ patch, web3, address, settings }) => {
-  const derived = await web3DeriveAccount(web3, address, patch);
-  try {
-    if (!derived) {
-      if (!patch) throw new Error("No derived account stored.");
-
-      return (
-        await axios.patch(process.env.REACT_APP_USER_SETTINGS_URL, {
-          payload: {
-            address,
-            settings,
-            signature: await web3.eth.personal.sign(JSON.stringify(settings), address),
-          },
-        })
-      ).data;
-    }
-
-    try {
-      return (
-        await axios[patch ? "patch" : "post"](process.env.REACT_APP_USER_SETTINGS_URL, {
-          payload: {
-            address,
-            settings,
-            signature: derived.sign(JSON.stringify(settings)).signature,
-          },
-        })
-      ).data;
-    } catch (err) {
-      if (!patch) throw new Error("No derived account stored.");
-
-      const settingsWithDerived = { ...settings, derivedAccountAddress: { S: derived.address } };
-      return (
-        await axios.patch(process.env.REACT_APP_USER_SETTINGS_URL, {
-          payload: {
-            address,
-            settings: settingsWithDerived,
-            signature: await web3.eth.personal.sign(JSON.stringify(settingsWithDerived), address),
-          },
-        })
-      ).data;
-    }
-  } catch (err) {
-    console.error(err.message);
-    return { error: "An unexpected error occurred." };
-  }
-};
-
 export const postJustification = async ({ web3, account, justification }) => {
   const derived = await web3DeriveAccount(web3, account, true);
   try {
